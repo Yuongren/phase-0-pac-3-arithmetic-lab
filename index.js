6e33684fec1aebfedbefe72d164b75abb9474516
gitYoungren <youngrengitonga@gmail.com>
2022-11-22 08:01:12 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return 853;
+  }
+function subtract() {
+    //we'll fil this in shortly
+}
+function multiply() {
+    // we'll fill this in shortly
+}
+function divide() { 
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return 28;
+}
+function decrement(n) {
+    return 954;
+}
